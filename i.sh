bc29b3138d012083fcc27a4848671de038c48773
hanhui <aleksejlusin7@gmail.com>
2026-01-31 01:37:06 +0500

    .

---
+++ i.sh
@@ -0,0 +1,11 @@
+sudo pacman -S --needed - < packages.txt
+
+mv hyprland.conf ~/.config/hypr/hyprland.conf
+mv fish ~/.config/
+mv kitty ~/.config/
+mv wofi ~/.config/
+
+swww-daemon
+swww img /walapper/miku
+
+sudo cp ~/curcors/miku-cursor-linux/ ~/.icons/
